94985b1080cf42d5eb2c9c1c7fa151722ebddeb0
saifur703 <saifur703@gmail.com>
2020-03-14 12:27:23 +0600

    cart area

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,25 @@ import './Cart.css';
 
 const Cart = props => {
   const cart = props.cart;
-  const total = cart.reduce((total, product) => total + product.price, 0);
+  const productPrice = cart.reduce(
+    (productPrice, product) => productPrice + product.price,
+    0
+  );
+  const tax = productPrice / 10;
+  const total = productPrice + tax;
+
+  const formatNumber = num => {
+    const number = num.toFixed(2);
+    return Number(number);
+  };
   return (
     <div>
       <h3>cart area text</h3>
       <h4>Order Summery: {cart.length}</h4>
 
-      <p>Total: {total}</p>
+      <p>Product Price: {formatNumber(productPrice)}</p>
+      <p>Vat: {formatNumber(tax)}</p>
+      <p>Total: {formatNumber(total)}</p>
     </div>
   );
 };
